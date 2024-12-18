58e2d479df97db1035d99795fb81e919683f4728
Mark Austen <117334224+Mark-Austen@users.noreply.github.com>
2024-12-18 13:21:01 +1100

    Update megaport.tf

--- megaport.tf
+++ megaport.tf
@@ -6,13 +6,6 @@ data "megaport_location" "megaport_location_2" {
   name              = var.megaport_location_2
 }
 
-data "megaport_partner" "aws_port_location_1" {
-  connect_type       = "AWS"
-  company_name       = "AWS"
-  product_name       = var.megaport_aws_port_location_1_name
-  location_id        = data.megaport_location.megaport_location_2.id
-}
-
 resource "megaport_mcr" "mcr_1" {
   product_name             = var.megaport_mcr_1_name
   location_id              = data.megaport_location.megaport_location_1.id
@@ -47,6 +40,14 @@ resource "megaport_vxc" "expressroute_vxc_1" {
   }
 }
 
+data "megaport_partner" "aws_port_location_1" {
+  connect_type   = "AWSHC"
+  company_name   = "AWS"
+  product_name   = var.megaport_aws_port_location_1_name
+  location_id    = data.megaport_location.megaport_location_2.id
+  diversity_zone = "red"
+}
+
 resource "megaport_vxc" "aws_vxc_1" {
   product_name         = var.megaport_aws_vxc_1_name
   rate_limit           = var.megaport_aws_vxc_1_bandwidth
@@ -54,6 +55,25 @@ resource "megaport_vxc" "aws_vxc_1" {
 
   a_end = {
     requested_product_uid = megaport_mcr.mcr_1.product_uid
+  }
+
+  a_end_partner_config = {
+    partner = "vrouter"
+    vrouter_config = {
+      interfaces = [
+        {
+          ip_addresses = ["192.168.50.1/30"]
+          bgp_connections = [
+            {
+              peer_asn         = 64512
+              local_ip_address = "192.168.50.1"
+              peer_ip_address  = "192.168.50.2"
+              password         = "<password>"
+            }
+          ]
+        }
+      ]
+    }
   }
 
   b_end = {
@@ -64,13 +84,18 @@ resource "megaport_vxc" "aws_vxc_1" {
     partner = "aws"
     aws_config = {
       name          = var.megaport_aws_vxc_1_name
-      asn           = var.megaport_mcr_1_asn
       type          = "private"
-      connect_type  = "AWS"
-      amazon_asn    = var.aws_dx_gateway_1_asn
+      connect_type  = "AWSHC"
       owner_account = var.aws_account_id
     }
   }
+}
+
+data "megaport_partner" "google_port_1_sin" {
+  connect_type = "GOOGLE"
+  company_name = "Google inc.."
+  product_name = "Singapore (sin-zone1-2260)"
+  location_id  = data.megaport_location.location_1.id
 }
 
 resource "megaport_vxc" "google_vxc_1" {
@@ -82,7 +107,9 @@ resource "megaport_vxc" "google_vxc_1" {
     requested_product_uid = megaport_mcr.mcr_1.product_uid
   }
 
-  b_end = {}
+  b_end = {
+    requested_product_uid = data.megaport_partner.google_port_1_sin.product_uid
+  }
 
   b_end_partner_config = {
     partner = "google"
